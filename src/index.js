084aa73aaf5d08a18b454801cd9f06f6980f8e9b
Dav <dafyddnash@gmail.com>
2023-02-20 17:51:26 +0000

    completed reveal on scroll

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import './style.css';
-import MobileMenu from './modules/js/MobileMenu';
+import './style.css'
+import MobileMenu from './modules/js/MobileMenu'
+import RevealOnScroll from './modules/js/RevealOnScroll'
+
+new RevealOnScroll(document.querySelectorAll(".featured-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
 
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
-  module.hot.accept();
-};
-
+  module.hot.accept()
+}
